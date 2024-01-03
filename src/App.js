aa0175387a01db490ad2c948d8b94431c88b4243
angaj <angaj@enfono.in>
2024-01-03 23:39:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer, useState } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const reducer = (state, action) => {
+  const { payload } = action;
+  switch (action.type) {
+    case "GET_TODOS": {
+      return {
+        todos: payload.todos,
+      };
+    }
+    // case "GET_IMAGES": {
+    //   return {
+    //     albums: state.albums,
+    //     images: payload.images,
+    //   };
+    // }
+    // case "ADD_ALBUM": {
+    //   return {
+    //     albums: [state.albums],
+    //   };
+    // }
+    // case "ADD_IMAGES": {
+    //   return {
+    //     albums: state.albums,
+    //     images: [state.images],
+    //   };
+    // }
+    // case "UPDATE_IMAGE": {
+    //   const imagesDuplicate = state.images;
+    //   imagesDuplicate[payload.ImagePos] = payload.image;
+    //   return {
+    //     albums: state.albums,
+    //     images: imagesDuplicate,
+    //   };
+    // }
+    // case "REMOVE_IMAGE": {
+    //   return {
+    //     albums: state.albums,
+    //     images: state.images.filter((image) => image.id !== payload.id),
+    //   };
+    // }
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { todos: [] });
+  const [loading, setLoading] = useState(false);
+
+  const getTodos = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const todos = await response.json();
+
+      dispatch({ type: "GET_TODOS", payload: { todos } });
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+
+    setLoading(false);
+  };
+
+  console.log(state);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Todo List</h2>
+      <TodoList todos={state.todos} />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,36 @@
+import { Col, Row, Form, Button } from "react-bootstrap";
+import styles from "../assets/css/TodoList.module.css";
+import { useState } from "react";
+
+const TodoList = ({ todos }) => {
+  const [showAdd, setShowAdd] = useState(false);
+
+  return (
+    <div className={styles.todo_container}>
+      <Button onClick={() => setShowAdd(!showAdd)}>Add Todo</Button>
+      {showAdd ? <div>Add Todo</div> : null}
+      {todos.map((todo) => (
+        <div className={styles.todo} key={todo.id}>
+          <Row>
+            <Col xs={9} className="text-center">
+              {" "}
+              <div>{todo.title}</div>
+            </Col>
+            <Col xs={3} className="text-center">
+              {" "}
+              <Form.Check
+                inline
+                checked={todo.completed}
+                name="group1"
+                type="checkbox"
+                // onClick={}
+              />
+            </Col>
+          </Row>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
